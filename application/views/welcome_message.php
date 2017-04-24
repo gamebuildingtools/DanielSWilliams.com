a3bdd1ccdb0476c2c2cd43268a0397769978b171
That's So Panda <thatssopanda@gmail.com>
2017-04-24 17:02:00 -0400

    Missed the js links

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -264,10 +264,10 @@
     </footer>
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="<?=base_url('assets/js/jquery.js')?>"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
 
     <!-- Custom Theme JavaScript -->
     <script>
