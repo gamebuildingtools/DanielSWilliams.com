fc9b5acf8a5caa6cdb5d61a8be6df581caa5cb77
That's So Panda <thatssopanda@gmail.com>
2017-04-25 16:30:21 -0400

    Minimized portfolio items and updated page content

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -166,7 +166,7 @@
     <!-- Callout -->
     <aside class="callout">
         <div class="text-vertical-center">
-            <h1>Vertically Centered Text</h1>
+            <h1>Web Developer, App Developer, Author.</h1>
         </div>
     </aside>
 
@@ -222,9 +222,9 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h3>The buttons below are impossible to resist.</h3>
-                    <a href="#" class="btn btn-lg btn-light">Click Me!</a>
-                    <a href="#" class="btn btn-lg btn-dark">Look at Me!</a>
+                    <h3>Want to collaborate on something? Let me know.</h3>
+                    <a href="mailto:me@danielswilliams.com" class="btn btn-lg btn-light">Email Me</a>
+                    <a href="https://twitter.com/gamebuilding" class="btn btn-lg btn-dark">Twitter</a>
                 </div>
             </div>
         </div>
