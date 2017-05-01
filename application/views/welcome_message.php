4fbca4f94539949811fe552e3bf4c895cea4c576
That's So Panda <thatssopanda@gmail.com>
2017-04-30 21:48:36 -0400

    Added favicon and updated some styles

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,10 +6,29 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="My corner of the web.">
+    <meta name="author" content="Daniel Williams">
 
     <title>DanielSWilliams.com</title>
+
+    <!-- My Favicon -->
+    <link rel="apple-touch-icon" sizes="57x57" href="<?=base_url('assets/img/favicon/apple-icon-57x57.png')?>">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?=base_url('assets/img/favicon/apple-icon-60x60.png')?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?=base_url('assets/img/favicon/apple-icon-72x72.png')?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url('assets/img/favicon/apple-icon-76x76.png')?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?=base_url('assets/img/favicon/apple-icon-114x114.png')?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?=base_url('assets/img/favicon/apple-icon-120x120.png')?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?=base_url('assets/img/favicon/apple-icon-144x144.png')?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?=base_url('assets/img/favicon/apple-icon-152x152.png')?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?=base_url('assets/img/favicon/apple-icon-180x180.png')?>">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?=base_url('assets/img/favicon/android-icon-192x192.png')?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?=base_url('assets/img/favicon/favicon-32x32.png')?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?=base_url('assets/img/favicon/favicon-96x96.png')?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?=base_url('assets/img/favicon/favicon-16x16.png')?>">
+    <link rel="manifest" href="<?=base_url('assets/img/favicon/manifest.json')?>">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?=base_url('assets/img/favicon/ms-icon-144x144.png')?>">
+    <meta name="theme-color" content="#ffffff">
 
     <!-- Bootstrap Core CSS -->
     <link href="<?=base_url('assets/css/bootstrap.min.css')?>" rel="stylesheet">
@@ -18,7 +37,7 @@
     <link href="<?=base_url('assets/css/stylish-portfolio.css')?>" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="<?=base_url('assets/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets/fonts/font-awesome/css/font-awesome.css')?>" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
